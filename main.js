277a0ff57230b367445f88bff0706fa4389e5cef
wxnyeki <frankwanyeki01@gmail.com>
2026-04-10 09:22:38 +0300

    Update and rename calculator.js to main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("display");
+
+function append(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
